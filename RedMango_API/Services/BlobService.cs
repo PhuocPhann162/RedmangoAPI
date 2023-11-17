f89fe2f597c753729de3681ceb47b810e184783f
Phuoc Phan <21520405@gm.uit.edu.vn>
2023-11-17 22:33:55 +0700

    get and delete blob method

--- RedMango_API/Services/BlobService.cs
+++ RedMango_API/Services/BlobService.cs
@@ -1,6 +1,37 @@
-﻿namespace RedMango_API.Services
+﻿
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+namespace RedMango_API.Services
 {
     public class BlobService : IBlobService
     {
+        private readonly BlobServiceClient _blobClient;
+        public async Task<bool> DeleteBlob(string blobName, string containerName)
+        {
+            BlobContainerClient blobContainerClient = _blobClient.GetBlobContainerClient(containerName);
+            BlobClient blobClient = blobContainerClient.GetBlobClient(blobName);
+
+            return await blobClient.DeleteIfExistsAsync();
+        }
+
+        public Task<string> GetBlob(string blobName, string containerName)
+        {
+            BlobContainerClient blobContainerClient = _blobClient.GetBlobContainerClient(containerName);
+            BlobClient blobClient = blobContainerClient.GetBlobClient(blobName);
+
+            return blobClient.Uri.AbsoluteUri;
+        }
+
+        public async Task<string> UploadBlob(string blobName, string containerName, IFormFile file)
+        {
+            BlobContainerClient blobContainerClient = _blobClient.GetBlobContainerClient(containerName);
+            BlobClient blobClient = blobContainerClient.GetBlobClient(blobName);
+            var httpHeaders = new BlobHttpHeaders()
+            {
+                ContentType = file.ContentType
+            };
+            var result = await blobClient.UploadAsync(file.OpenReadStream(), httpHeaders);
+        }
     }
 }
