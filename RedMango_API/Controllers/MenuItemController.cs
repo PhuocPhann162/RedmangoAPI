24fe2c60d230db3c398eae93953217f702b7edbd
Phuoc Phan <21520405@gm.uit.edu.vn>
2023-11-17 21:54:06 +0700

    initial project and add GET method for MenuItem

---
+++ RedMango_API/Controllers/MenuItemController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RedMango_API.Data;
+using RedMango_API.Models;
+using System.Net;
+
+namespace RedMango_API.Controllers
+{
+    [Route("api/MenuItem")]
+    [ApiController]
+    public class MenuItemController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        private ApiResponse _response;
+        public MenuItemController(ApplicationDbContext db)
+        {
+            _db = db;
+            _response = new ApiResponse();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMenuItems()
+        {
+            _response.Result =  _db.MenuItems;
+            _response.StatusCode = HttpStatusCode.OK;
+            return Ok(_response);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetMenuItem(int id)
+        {
+            if(id == 0)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                return BadRequest(_response);
+            }
+            MenuItem menuItem = _db.MenuItems.FirstOrDefault(u => u.Id == id);
+            if (menuItem == null)
+            {
+                _response.StatusCode = HttpStatusCode.NotFound;
+                return NotFound(_response);
+            }
+            _response.Result = _db.MenuItems.FirstOrDefault(u => u.Id == id);
+            _response.StatusCode = HttpStatusCode.OK;
+            return Ok(_response);
+        }
+    }
+}
